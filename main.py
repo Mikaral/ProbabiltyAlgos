133c64e126e9c972344193befb8fdb15997e820f
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2021-08-25 21:20:48 -0300

    Implementation of several distributions for both discrete and continuous variables

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 import math
 import sympy as sp
+from sympy import oo, exp
 
-x, y, z = sp.symbols('x y z')
+x = sp.symbols('x')
 
 
 # Auxiliary function that returns the square of the parameter j
@@ -14,7 +15,7 @@ def power(j):
 # Implementation of the Cumulative Distribution Function, which "interval" is an 2-size array
 # of the interval used on the integral and expr is a string expressing the function.
 def cumulative_distribution_continuous(interval, expr):
-    return sp.integrate(expr, (x, interval[0], interval[1]))
+    return sp.integrate(sp.sympify(expr), (x, interval[0], interval[1]))
 
 
 # Implementation of expected values on discrete variables, which "x_values" are all the Values of x
@@ -35,10 +36,7 @@ def expected_value(x_values, probs_x):
 # Implementation of expected values on continuous variables, which "interval" is an 2-size array
 # of the interval used on the integral and expr is a string expressing the function.
 def expected_value_continuous(interval, expr):
-    expr += "*x"
-    expr = sp.sympify(expr)
-
-    return sp.integrate(expr, (x, interval[0], interval[1]))
+    return sp.integrate(sp.sympify(expr), (x, interval[0], interval[1]))
 
 
 # Implementation of Variance on discrete variables, which "x_values" are all the Values of x
@@ -103,3 +101,103 @@ def expected_value_binomial(n, p):
 # is the probability of success
 def variance_binomial(n, p):
     return n * p * (1 - p)
+
+
+#  - Geometric Distribution -
+
+def geometric_distribution_s(p, k):
+    return math.pow(1 - p, k - 1) * p
+
+
+def geometric_distribution_f(p, y):
+    return math.pow(1 - p, y) * p
+
+
+def expected_value_geometric_s(p):
+    return 1 / p
+
+
+def expected_value_geometric_f(p):
+    return (1 - p) / p
+
+
+def variance_geometric(p):
+    return (1 - p) / p ** 2
+
+
+# - Poisson Distribution
+
+def poisson_distribution(lmbd, k):
+    return (math.pow(math.e, -lmbd) * math.pow(lmbd, k)) / math.factorial(k)
+
+
+def expected_value_poisson(lmbd):
+    return lmbd
+
+
+def variance_poisson(lmbd):
+    return lmbd
+
+
+# - Uniform Model
+
+def density_uniform_distribution(a, b):
+    return 1 / (b - a)
+
+
+def cumulative_uniform_distribution(z, a, b):
+    return (z - a) / (b - a)
+
+
+def expected_value_uniform(a, b):
+    return (a + b) / 2
+
+
+def variance_uniform(a, b):
+    return math.pow(b - a, 2) / 12
+
+
+# Exponential Model - When the pdf is given by a*e^(-ax)
+
+def exponential_model_morethan(a, z):
+    return a * math.pow(math.e, -a * z)
+
+
+def exponential_model_lessthan(a, z):
+    return 1 - math.pow(math.e, -a * z)
+
+
+def expected_value_exponential(a):
+    return 1 / a
+
+
+def variance_exponential(a):
+    return 1 / (a ** 2)
+
+
+# - Gama Function -
+
+def gama(p):
+    return sp.integrate(x ** (p - 1) * exp(-x), (x, 0, oo))
+
+
+def gama_integer(n):
+    return math.factorial(n - 1)
+
+
+def pdf_gama(a, r, z):
+    g = gama(r)
+    return sp.integrate((a / g) * (a * x) ** (r - 1) * exp(-a * x), (x, 0, z))
+
+
+def pdf_gama_integer(a, r, z):
+    g = gama_integer(r)
+    return sp.integrate((a / g) * (a * x) ** (r - 1) * exp(-a * x), (x, 0, z))
+
+
+def expected_value_gama(a, r):
+    return r / a
+
+
+def variance_gama(a, r):
+    return r / (a ** 2)
